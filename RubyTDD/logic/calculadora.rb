3c64a01ef4d14dafafa7582a230510330f9b442e
Gabriel Vanz <gabrivanz@hotmail.com>
2022-07-26 13:45:49 -0300

    Adição do método de subtração à calculadora

--- RubyTDD/logic/calculadora.rb
+++ RubyTDD/logic/calculadora.rb
@@ -2,5 +2,9 @@ class Calculadora
     def soma (a,b)
         a+b
     end
-    
+        
+    def subtrai (a,b)
+        a-b
+    end    
+
 end
--- RubyTDD/specs/calculadora_spec.rb
+++ RubyTDD/specs/calculadora_spec.rb
@@ -2,6 +2,8 @@ require 'rspec'
 require_relative '../logic/calculadora'
 
 describe('Calculadora') do
+
+    #Teste com o método de soma
     it('Deve somar dois números inteiros positivos') do
         calculadora = Calculadora.new
         expect(calculadora.soma(4,5)).to eq 9
@@ -12,13 +14,51 @@ describe('Calculadora') do
         expect(calculadora.soma(9,0)).to eq 9
     end
 
-    it('Deve somar números negativos') do
+    it('Deve somar dois números inteiros negativos') do
         calculadora = Calculadora.new
         expect(calculadora.soma(-9,-10)).to eq -19
+    end
+
+    it('Deve somar um número negativo com um número positivo') do
+        calculadora = Calculadora.new
+        expect(calculadora.soma(-2,20)).to eq 18
     end
 
     it('Deve somar números decimais') do
         calculadora = Calculadora.new
         expect(calculadora.soma(5.5,11.1)).to eq 16.6
     end
+
+
+    #Teste com o método de subtração
+    it('Deve subtrair dois números inteiros positivos') do
+        calculadora = Calculadora.new
+        expect(calculadora.subtrai(10,5)).to eq 5
+    end
+
+    it('Deve subtrair um número qualquer com 0') do
+        calculadora = Calculadora.new
+        expect(calculadora.subtrai(2,0)).to eq 2
+    end
+
+    it('Deve subtrair dois números inteiros negativos') do
+        calculadora = Calculadora.new
+        expect(calculadora.subtrai(-8,-6)).to eq -2
+    end
+
+    it('Deve subtrair um número negativo com um número positivo') do
+        calculadora = Calculadora.new
+        expect(calculadora.subtrai(-10,4)).to eq -14
+    end
+
+    it('Deve subtrair números inteiros, sendo o primeiro número menor que o segundo') do
+        calculadora = Calculadora.new
+        expect(calculadora.subtrai(4,16)).to eq -12
+    end
+
+    it('Deve subtrair números decimais') do
+        calculadora = Calculadora.new
+        expect(calculadora.subtrai(5.8,1.5)).to eq 4.3
+    end
+
 end
